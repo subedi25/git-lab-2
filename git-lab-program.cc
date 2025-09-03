9df59c8752048a5e1ab574cfe2495bd22cb53572
Nia <subediananya25@gmail.com>
2025-09-03 18:28:13 -0400

    assignment

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Nia Subedi
+ *        Date: Sep 3rd 2025
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "hello Git!!" << endl;
+    return 0;
+}// main
